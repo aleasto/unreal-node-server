4efb7983ec2b574f28e8735d11e3ac8b408b1c8d
Alessandro Astone <ales.astone@gmail.com>
2020-10-21 02:01:36 +0200

    Active users count

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const TIMEOUT_SECONDS = 10;
+const users = {};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if (req.query.id) {
+    users[req.query.id] = Date.now();
+  }
+
+  res.json({ active: Object.keys(users).length });
 });
+
+setInterval(() => {
+  let now = Date.now();
+  for (let user in users) {
+    if (now - users[user] >= TIMEOUT_SECONDS * 1000) {
+      delete users[user];
+    }
+  }
+}, TIMEOUT_SECONDS * 1000);
 
 module.exports = router;
